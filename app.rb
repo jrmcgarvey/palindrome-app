7d0c6dd31f35aa2aad4ef01027a1530589d093c5
John McGarvey <jmcgarve@bellsouth.net>
2018-09-14 02:09:07 +0000

    Finish palindrome detector

--- app.rb
+++ app.rb
@@ -1,7 +1,25 @@
 require 'sinatra'
+require 'rerun'
+require 'mhartl_palindrome'
 
 set :bind, '0.0.0.0'
 
 get '/' do
-  'hello, world!'
+  @title = 'Home'
+  erb :index
+end
+
+get '/about' do
+  @title = 'About'
+  erb :about
+end
+
+get '/palindrome' do
+  @title = 'Palindrome Detector'
+  erb :palindrome
+end
+
+post '/check' do
+  @phrase = params[:phrase]
+  erb :result
 end
